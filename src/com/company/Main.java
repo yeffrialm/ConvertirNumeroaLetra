f7419c353ec7d57a5db7369d82f7bf7bdfe1a2f8
yeffrialm <43483558+yeffrialm@users.noreply.github.com>
2020-09-09 16:07:18 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         NumeroaLetra miNumeroaLetra = new NumeroaLetra();
         miNumeroaLetra.presentacionInicio();
-        miNumeroaLetra.EntradaDatos();
-        miNumeroaLetra.MostrarEnLetra();
+        miNumeroaLetra.entradaDatos();
+        miNumeroaLetra.mostrarEnLetra();
     }
 }
